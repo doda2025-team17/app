d47cfd3f1e76952bb36975091380bd4f4f3ba7d4
Nicholas <nicholaswu2003@gmail.com>
2025-11-21 00:35:33 +0100

    F1 completed by adding version in app

--- src/main/java/frontend/ctrl/HelloWorldController.java
+++ src/main/java/frontend/ctrl/HelloWorldController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import nl.tudelft.doda.VersionUtil;
+
 @Controller
 public class HelloWorldController {
 
     @GetMapping("/")
     @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "Hello World! Version: " + VersionUtil.getVersion();
     }
 }
